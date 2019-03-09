4f560b140389131de89e9268aaa7dc2ef88758b4
Lucas Damas <suporte@cloudmed.io>
2019-03-09 15:33:42 -0300

    adicionei todas as tabelas mas estão comentadas para testes

--- js/maintable.js
+++ js/maintable.js
@@ -1,39 +1,114 @@
-var teste = sessionStorage.getItem('array-finalzao')
-        console.log(JSON.parse(teste));
-        
-        var ultimo = JSON.parse(teste).length
-        let body = "";
-        $("#tbodyestado").innerHTML = "";
-        for (var i = 0; i < ultimo; i++) {
 
-          body +=
-            "<tr>" +
-                "<td  class='text-susses'>"+
-                    JSON.parse(teste)[i].RA +
-                  "</td>"+
-                  "<td>"+
-                      JSON.parse(teste)[i].ESTADO +
-                  "</td>"+
-                  "<td>"+
-                      JSON.parse(teste)[i].CIDADESP +
-                  "</td>"+
-                  "<td>"+
-                      JSON.parse(teste)[i].COMQUEMMORA2 +
-                  "</td>"+
-                  "<td class='text-primary'>"+
-                      JSON.parse(teste)[i].QUALADISTÂNCIADOTRABALHOCASAATÉAFACULDADE2 +
-                  "</td>"+
-          "<tr>";
-              var tr = document.getElementsByTagName("td");
-              
-        };
-        document.getElementById("tbodyestado").innerHTML = body;
-      // http://www.chartjs.org/docs/#doughnut-pie-chart-introduction
+var teste = sessionStorage.getItem('array-finalzao')
+console.log(JSON.parse(teste));
+
+var ultimo = JSON.parse(teste).length
+let body = "";
+$("#tbodyestado").innerHTML = "";
+var array = JSON.parse(teste);
+for (var i = 0; i < ultimo; i++) {
+    if (i == 0) {
+        body +=
+            "<th>" +
+            "Id" +
+            "</th>" +
+            "<th>" +
+            "ESTADO" +
+            "</th>" +
+            "<th>" +
+            "CIDADE" +
+            "</th>" +
+            "<th>" +
+            "COM QUEM MORA" +
+            "</th>" +
+            "<th>" +
+            "AQUEESSECONHECIMENTOSERELACIONA" +
+            "</th>" +
+            "<th>" +
+            "DATADENASCIMENTO" +
+            "</th>";
+
+    }
+
+    body +=
+        "<tr>" +
+        "<td class='text-primary'>" +
+        array[i].RA +
+        "</td>" +
+        "<td id='tableEstado'>" +
+        array[i].ESTADO +
+        "</td>" +
+        "<td id='tableEstado'>" +
+        array[i].CIDADESP +
+        "</td>" +
+        "<td id='tableEstado'>" +
+        array[i].COMQUEMMORA2 +
+        "</td>" +
+        "<td>" +
+        array[i].AQUEESSECONHECIMENTOSERELACIONA +
+        "</td>" +
+        "<td>"+
+      array[i].DATADENASCIMENTO +
+      "</td>";
+    //   "<td>"+
+    //   array[i].EMQUAISESCOLASESTUDOU2 +
+    //   "</td>"+
+    //   "<td>"+
+    //   array[i].ESTADOCÍVIL2 +
+    //   "</td>"+
+    //   "<td>"+
+    //   array[i].PERÍODO +
+    //   "</td>"+
+    //   "<td>"+
+    //   array[i].PORQUESECANDIDATOUAUMAVAGANAFATECFRANCA2 +
+    //   "</td>"+
+    //   "<td>"+
+    //   array[i].QUALARENDAFAMILIAR2 +
+    //   "</td>"+
+    //   "<td>"+
+    //   array[i].QUALARENDAFAMILIAR2 +
+    //   "</td>"+
+    //   "<td>"+
+    //   array[i].QUANTOTEMPOPORSEMANAVOCÊDEDICAESTUDANDOOUAPRIMORANDOSUASHABILIDADES +
+    //   "</td>"+
+    //   "<td>"+
+    //   array[i].QUETRANSPORTEUSAPARAIRPARAAFACULDADE2 +
+    //   "</td>"+
+    //   "<td>"+
+    //   array[i].SEXOGÊNERO +
+    //   "</td>"+
+    //   "<td>"+
+    //   array[i].SUARESIDÊNCIAÉ +
+    //   "</td>"+
+    //   "<td>"+
+    //   array[i].TEMACESSOAATENDIMENTODESAÚDE +
+    //   "</td>"+
+    //   "<td>"+
+    //   array[i].TEMFILHOS +
+    //   "</td>"+
+    //   "<td>"+
+    //   array[i].VOCÊJÁFEZOUTROCURSO +
+    //   "</td>"+
+    //   "<td>"+
+    //   array[i].TEMFILHOS +
+    //   "</td>"+
+    //   "<td >"+
+    //   array[i].VOCÊTRABALHAQUALOREGIMEDETRABALHO +
+    //   "</td>"+
+    //   "<td >"+
+    //   array[i].ÉPORTADORDENECESSIDADESESPECIAIS +
+    //   "</td>"+
+    "<tr>";
+    var tr = document.getElementsByTagName("td");
+
+};
+document.getElementById("theadestado").innerHTML = body;
+// http://www.chartjs.org/docs/#doughnut-pie-chart-introduction
 
 var data = [
     {
         value: 30,
-        color:"#F7464A",
+        color: "#F7464A",
         highlight: "#FF5A5E",
         label: "Red"
     },
@@ -63,7 +138,7 @@ new Chart(ctx).Pie(data);
 var data2 = [
     {
         value: 30,
-        color:"#F7464A",
+        color: "#F7464A",
         highlight: "#FF5A5E",
         label: "Red"
     },
